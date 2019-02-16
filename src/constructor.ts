94ffacd4968696b012b215ccf70496b569e37889
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2019-02-16 15:08:22 +0100

    Add support for class methods

--- src/constructor.ts
+++ src/constructor.ts
@@ -5,6 +5,15 @@ export function constructInterface(cd: ClassDeclaration) {
   const classProps = cd.properties.map(p => {
     return `  ${p.name}${p.isOptional ? '?' : ''}: ${p.type};`;
   }).join('\n');
+  const classMethods = cd.methods
+    .filter(m => { return m.visibility !== 0; })
+    .map(m => {
+      const params = m.parameters.map(p => {
+        return `${p.name}: ${p.type}`;
+      }).join(', ');
 
-  return `interface I${className} {\n${classProps}\n}\n`;
+      return `  ${m.name}${params.length > 0 ? `(${params})` : ''}: ${m.type};`;
+    }).join('\n');
+
+  return `interface I${className} {\n${classProps}\n${classMethods}\n}\n`;
 }
