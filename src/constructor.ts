08a65ab3c350587ed8c7305d0cec0210d7bb7225
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2019-02-23 13:04:22 +0100

    Remove static and private props from interface

--- src/constructor.ts
+++ src/constructor.ts
@@ -5,7 +5,9 @@ export interface IConstructedInterface {
   value: string;
 }
 
-export function constructInterface(cd: ClassDeclaration): IConstructedInterface {
+export function constructInterface(
+  cd: ClassDeclaration
+): IConstructedInterface {
   const className = cd.name.charAt(0).toUpperCase() + cd.name.substring(1);
   const classProps = getProperties(cd);
   const classMethods = getMethods(cd);
@@ -17,19 +19,29 @@ export function constructInterface(cd: ClassDeclaration): IConstructedInterface 
 }
 
 function getProperties(cd: ClassDeclaration) {
-  return cd.properties.map(p => {
-    return `  ${p.name}${p.isOptional ? '?' : ''}: ${p.type};\n`;
-  }).join('');
+  return cd.properties
+    .map(p => {
+      return p.visibility !== 0 && !p.isStatic
+        ? `  ${p.name}${p.isOptional ? "?" : ""}: ${p.type};\n`
+        : "";
+    })
+    .join("");
 }
 
 function getMethods(cd: ClassDeclaration) {
   return cd.methods
-    .filter(m => { return m.visibility !== 0; })
+    .filter(m => {
+      return m.visibility !== 0;
+    })
     .map(m => {
-      const params = m.parameters.map(p => {
-        return `${p.name}: ${p.type || "void"}`;
-      }).join(', ');
+      const params = m.parameters
+        .map(p => {
+          return `${p.name}: ${p.type || "void"}`;
+        })
+        .join(", ");
 
-      return `  ${m.name}(${params.length > 0 ? `${params}` : ''}): ${m.type || "void"};\n`;
-    }).join('');
+      return `  ${m.name}(${params.length > 0 ? `${params}` : ""}): ${m.type ||
+        "void"};\n`;
+    })
+    .join("");
 }
